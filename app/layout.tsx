94db5a1a3e1f04c7d0dce00dcf9403d5284a4174
Redemption19 <abbanb2@gmail.com>
2025-01-15 00:33:47 +0000

    first-commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
+
+const font = Space_Grotesk({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Learning with WDW ",
+  description: "E-learning website using next js ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ResponsiveNav />
+        {children}
+        <Footer />
+        <ScrollToTop />
+      </body>
+    </html>
+  );
+}
---
+++ components/Home/About/About.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { FaArrowRight, FaAward } from "react-icons/fa";
+
+const About = () => {
+  return (
+    <div className="pt-16 pb-16 ">
+      {/* define grid */}
+      <div className="w-4/5 mx-auto grid grid-cols-1 lg:grid-cols-2 items-center gap-16">
+        {/* 1st part */}
+        <div data-aos="fade-right" data-aos-anchor-placement="top-center">
+          <div className="flex items-center space-x-4">
+            <div className="w-12 h-12 bg-rose-600 rounded-full flex items-center justify-center flex-col">
+              <FaAward className="h-6 w-6 text-white" />
+            </div>
+            <h1 className="text-xl text-black font-semibold">
+              Guaranteed and certified
+            </h1>
+          </div>
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl mt-8 font-bold md:leading-[3rem] lg:leading-[3.5rem] xl:leading-[3.9rem] text-gray-800">
+            Online learning wherever and whenever.
+          </h1>
+          <p className="mt-4 text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+            non repellendus aliquam, dicta blanditiis voluptatum autem error
+            culpa inventore.{" "}
+          </p>
+          <button className="flex items-center space-x-2 px-8 py-3 mt-8 hover:bg-gray-700 transition-all duration-200 rounded-3xl bg-black text-white">
+            <span>Learn More</span>
+            <FaArrowRight />
+          </button>
+        </div>
+        {/* 2nd part */}
+        <div
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="150"
+        >
+          <div>
+            <h1 className="text-7xl lg:text-9xl font-bold text-black text-opacity-5">
+              01
+            </h1>
+            <div className="-mt-10">
+              <h1 className="text-xl md:text-2xl text-opacity-70 mb-3 text-black font-bold">
+                Flexible schedule
+              </h1>
+              <p className="w-[90%] lg:w-[70%] text-base text-black text-opacity-60">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
+                ipsum dolor sit amet. Temporibus, similique?
+              </p>
+            </div>
+          </div>
+          <div className="mt-8 w-full">
+            <h1 className="text-7xl lg:text-9xl font-bold text-black text-opacity-5">
+              02
+            </h1>
+            <div className="-mt-10">
+              <h1 className="text-xl md:text-2xl text-opacity-70 mb-3 text-black font-bold">
+                Pocket Friendly
+              </h1>
+              <p className="w-[90%] lg:w-[70%] text-base text-black text-opacity-60">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
+                ipsum dolor sit amet. Temporibus, similique?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
